78255207c7bc02669132a69fd551e1cc2214b858
phlco <phillip.lamplugh@gmail.com>
2014-03-30 18:27:41 -0400

    adds testing

--- spec/monocular_spec.rb
+++ spec/monocular_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 require 'monocular'
 
 describe Monocular do
-  let(:monocular) { Monocular::Moncular.new }
+  let(:monocular) { Monocular::Monocular.new }
   describe '#extract_name' do
     it 'returns the name of a gem from a line' do
       line = "gem 'ruby-openid', :git => 'git://github.com/kendagriff/ruby-openid.git', :ref => '79beaa419d4754e787757f2545331509419e222e'"
-      monocular.extract_name(line)
+      name = monocular.extract_name(line)
       expect( name ).to eq 'ruby-openid'
     end
   end
@@ -18,22 +18,34 @@ describe Monocular do
   end
   describe '#description_from' do
     context('When Gem has a description') do
-      gem_spec = double('Gem::Specificiation', :description => 'Sass adapter for the Rails asset pipeline.')
-      desc = monocular.description_from(gem_spec)
-      expect( desc ).to eq 'Sass adapter for the Rails asset pipeline.'
+      it "returns description" do
+        gem_spec = double('Gem::Specificiation', :description => 'Sass adapter for the Rails asset pipeline.')
+        desc = monocular.description_from(gem_spec)
+        expect( desc ).to eq 'Sass adapter for the Rails asset pipeline.'
+      end
     end
     context('when Gem only has a summary') do
-      gem_spec = double('Gem::Specificiation', :description => '', :summary => 'Sass adapter for the Rails asset pipeline.')
-      desc = monocular.description_from(gem_spec)
-      expect( desc ).to eq 'Sass adapter for the Rails asset pipeline.'
+      it "returns the summary" do
+        gem_spec = double('Gem::Specificiation', :description => '', :summary => 'Sass adapter for the Rails asset pipeline.')
+        desc = monocular.description_from(gem_spec)
+        expect( desc ).to eq 'Sass adapter for the Rails asset pipeline.'
+      end
     end
     context('when neither') do
-      desc = monocular.description_from(gem_spec)
-      expect( desc ).to eq 'No Description Available'
+      it "returns no description available" do
+        gem_spec = double('Gem::Specificiation', :description => '', :summary => '')
+        desc = monocular.description_from(gem_spec)
+        expect( desc ).to eq 'No Description Available'
+      end
     end
   end
   describe '#word_wrap' do
     it 'formats text in 80 character lines' do
+      unformatted_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      formatted_text = monocular.word_wrap(unformatted_text)
+      formatted_text.each_line do |line|
+        expect( line.length ).to be < 100
+      end
     end
   end
 end
