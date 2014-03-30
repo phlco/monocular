5daf9716a4f542459e369457600925820ae2cae7
phlco <phillip.lamplugh@gmail.com>
2014-03-30 17:03:12 -0400

    refactoring

--- lib/monocular.rb
+++ lib/monocular.rb
@@ -4,18 +4,21 @@ require 'date'
 
 module Monocular
   class Monocular
-
     attr_accessor :gem_lines
 
     def initialize
       @gem_lines = []
     end
 
-    def annotate!
-      backup_gemfile
-      parse_gemfile
-      annotate_gems
-      write_to_gemfile
+    def self.annotate!
+      puts "Annotating Gemfile"
+      new do |m|
+        m.backup_gemfile
+        m.parse_gemfile
+        m.annotate_gems
+        m.write_to_gemfile
+      end
+      puts "Done"
     end
 
     def backup_gemfile
@@ -23,7 +26,6 @@ module Monocular
     end
 
     def parse_gemfile
-      gem_lines = []
       File.read('Gemfile').each_line do |line|
         # Skip commented lines
         next if line.start_with?('#')
@@ -32,12 +34,11 @@ module Monocular
       end
     end
 
-    # annotate the gem file with title, description and gem
     def annotate_gems
       gem_lines.map! do |line|
         gem_name = extract_name(line)
         if gem_name
-          [describe(gem_name), line, ''].join('\n')
+          [word_wrap(description_from(specs_for(gem_name))), line, ''].join("\n")
         else
           line
         end
@@ -47,32 +48,21 @@ module Monocular
     def write_to_gemfile
       File.open('Gemfile', 'w+') do |f|
         f.puts("# == Annotated on #{Date.today} ==")
-        gem_lines.each do |line|
-          f.puts(line)
-        end
+        gem_lines.each { |line| f.puts(line) }
       end
     end
 
-    # parses name from line in Gemfile
     def extract_name(line)
       line.scan(/gem '(.*?)',?/).flatten.first
+      print "."
     end
 
-    # form the commented out describe block
-    def describe(gem_name)
-      gem_specs = specs_for(gem_name)
-      description = description_from(gem_specs)
-      word_wrap(description)
-    end
-
-    # Gem specifications are read in as YAML
     def specs_for(gem_name)
       spec = YAML.load(`gem specification #{gem_name}`)
       return spec if spec
       Gem.latest_spec_for(gem_name)
     end
 
-    # Get the description or summary of a gem if available
     def description_from(gem_spec)
       if gem_spec.respond_to?(:description) && gem_spec.description
         gem_spec.description
@@ -83,18 +73,18 @@ module Monocular
       end
     end
 
-    # limit lines to 80 characters
     def word_wrap(description)
       # split on new line or space
       words = description.split(/[\n|\s]/)
       lines = []
       while words.any?
         line = '# '
-        line << "#{words.shift} " until line.size >= 80
+        line  << "#{words.shift} " until line.size >= 80
         lines << line
       end
-      lines.join('\n')
+      lines.join("\n")
     end
-
   end
 end
+
+Monocular::Monocular.annotate!
